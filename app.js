120739d285b2a3ce8e3ad40b319e26274c08c2e9
gatoncyrhadenice@gmail.com <gatoncyrhadenice@gmail.com>
2022-10-18 15:30:41 +0800

    add.js, present.js updated

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const write = require ('./write');
-const cmd = process.argv[2]
-//write (cmd);
+const add = require('./add')
+const read = require('./read')
+const present = require('./present')
+//get user input
+const cmd = process.argv
 
-const read = require ('./read');
-console.log(read());
+var note ={}
+if (cmd[2] == 'add'){
+    //Build Object
+    note = {
+        id: cmd[3],
+        title: cmd[4],
+        body: cmd[5]
+    }
+    //get old note value
+    var oldNote = read()
+    //add note to note
+    add(note,oldNote)
+}
+if (cmd[2] == 'read') {
+    present(read())
+}
